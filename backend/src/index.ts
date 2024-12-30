3cbb569baa0ac37b6fb5edf42a359229b1031655
VÕ MINH THÀNH - Service Core <codera0001@gmail.com>
2024-12-30 23:34:57 +0700

    Ok

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,16 @@
-import express, { Request, Response } from 'express';
-import { Server } from 'socket.io';
-const { createServer } = require('http');
-import { getLatestChat } from './utilities';
-import { ChangeStreamDocument } from 'mongodb';
+import express, {Request, Response} from 'express';
+import {Server} from 'socket.io';
+
+const {createServer} = require('http');
+import {getDatabaseVitualsBackroom, getLatestChat} from './utilities';
+import {ChangeStreamDocument} from 'mongodb';
 
 const server = createServer()
 const io = new Server(server, {
   cors: {
-    origin: process.env.NODE_ENV === 'production' 
-      ? process.env.FRONTEND_URL || '*.ondigitalocean.app'
-      : "http://localhost:3000",
+    origin: process.env.NODE_ENV === 'production'
+        ? process.env.FRONTEND_URL || '*.ondigitalocean.app'
+        : "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -23,34 +24,66 @@ let changeStreamInitialized = false;
 
 io.on('connection', (socket) => {
   const sessionId = socket.handshake.auth.sessionId;
-  
+
   if (sessionId) {
     socket.data.sessionId = sessionId;
     console.log('Reconnected client with session ID:', sessionId);
   } else {
     console.log('New client connected:', socket.id);
   }
-  
+
+  console.log("Done", changeStreamInitialized)
+
   // Only setup change stream once
   if (!changeStreamInitialized) {
-    setupChangeStream();
+    setupChangeStream().then(r => console.log("SetupChangeStream is running"));
     changeStreamInitialized = true;
   }
 });
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function setupChangeStream() {
-  const chat = await getLatestChat();
-  const changeStream = chat.watch();
-  changeStream.on('change', (change: ChangeStreamDocument<any>) => {
-    if (change.operationType === 'insert' && 'fullDocument' in change) {
-      const content = JSON.stringify(change.fullDocument.content);
-      const timestamp = JSON.stringify(change.fullDocument.timestamp);
-      const role = JSON.stringify(change.fullDocument.role);
-      if (io) {
-        io.emit('newDocument', timestamp, content, role);
+  let dbChangeStream;
+  try {
+    const db = await getDatabaseVitualsBackroom();
+    const collections = await db.listCollections().toArray();
+    const sortedCollections = collections.sort((a, b) => b.name.localeCompare(a.name));
+    dbChangeStream = db.watch();
+    let latestCollectionName = sortedCollections[0].name;
+
+    dbChangeStream.on('change', (change: ChangeStreamDocument<any>) => {
+      console.log(JSON.stringify(change));
+      if (change.operationType === 'insert' && 'fullDocument' in change && 'ns' in change) {
+        const currentCollection = change.ns.coll;
+        console.log(latestCollectionName, currentCollection, latestCollectionName.localeCompare(currentCollection));
+        if (latestCollectionName.localeCompare(currentCollection) <= 0) {
+          latestCollectionName = currentCollection;
+          const content = JSON.stringify(change.fullDocument.content);
+          const timestamp = JSON.stringify(change.fullDocument.timestamp);
+          const role = JSON.stringify(change.fullDocument.role);
+          if (io) {
+            console.log(timestamp, role, content);
+            io.emit('newDocument', timestamp, content, role);
+          }
+        } else {
+          console.log("Old collection", change);
+        }
+      } else {
+        console.log("Change event does not have a fullDocument or is not an insert operation:", change);
       }
-    } else {
-      console.log("Change event does not have a fullDocument or is not an insert operation:", change);
+    });
+  } catch (e) {
+    console.log("Has exception", e)
+    if (dbChangeStream != null) {
+      console.log("dbChangeStream is close")
+      dbChangeStream.close();
     }
-  });
+
+    await sleep(5000)
+    // Re init change stream
+    await setupChangeStream();
+  }
 }
