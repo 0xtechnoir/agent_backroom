4be2650c3af324bb70e13e8ab71c6d8da4b3b0a2
VÕ MINH THÀNH - Service Core <codera0001@gmail.com>
2025-01-05 14:11:06 +0700

    Ok

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const server = createServer()
 const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === 'production'
-        ? process.env.FRONTEND_URL || '*.ondigitalocean.app'
+        ? process.env.FRONTEND_URL || process.env.FRONTEND_URL
         : "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
 
   // Only setup change stream once
   if (!changeStreamInitialized) {
-    setupChangeStream().then(r => console.log("SetupChangeStream is running"));
+    setupChangeStream().then(() => console.log("SetupChangeStream is running"));
     changeStreamInitialized = true;
   }
 });
@@ -64,7 +64,7 @@ async function setupChangeStream() {
           const timestamp = JSON.stringify(change.fullDocument.timestamp);
           const role = JSON.stringify(change.fullDocument.role);
           if (io) {
-            console.log(timestamp, role, content);
+            console.log(timestamp, role);
             io.emit('newDocument', timestamp, content, role);
           }
         } else {
